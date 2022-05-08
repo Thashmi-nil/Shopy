8b23260a77bb536e50c9c024427a61dce99bdfda
Nilupuli Sanjana <nilupuleethash765@gmail.com>
2022-05-09 00:24:10 +0530

    make responsive for mobile view

--- shop-frontend/src/components/RightSide/RightSide.jsx
+++ shop-frontend/src/components/RightSide/RightSide.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import CustomerReview from '../Cards/CustomerReview'
+import CustomerReview from '../CustomerReview/CustomerReview'
 import Updates from '../Updates/Updates'
 import './RightSide.css'
 
 const RightSide = () => {
     return (
-        <div className="ightSide">
+        <div className="RightSide">
             <div>
                 <h3>Updates</h3>
                 <Updates />
